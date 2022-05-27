d4f98988b45a6cba5bd6231abad0a0392b8fd827
valzagrainca <75394963+valzagrainca@users.noreply.github.com>
2022-05-27 22:38:01 +0200

    Connect GET,POST,PATCH  of booking  to the client side

--- Client/src/DataAccess/BookingData.java
+++ Client/src/DataAccess/BookingData.java
@@ -1,9 +1,17 @@
 package DataAccess;
 
 import Application.DataTypes.Booking;
+import Application.DataTypes.Plane;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -16,32 +24,42 @@ public class BookingData {
     //fields
     private static Statement statement;
     private static ObservableList<Booking> bookings;
+    private static String url = "http://127.0.0.1:8000/api/booking/";
 
 
     //get bookings
     public static ObservableList<Booking> getBookings(){
         bookings = FXCollections.observableArrayList();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        try {
+            HttpResponse<String> response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());
+            //  System.out.println(response.body());
+            JSONArray booking=new JSONArray(response.body());
+            for(int i=0;i<booking.length();i++){
+                JSONObject bookingobj= booking.getJSONObject(i);
+                int booking_id=bookingobj.getInt("booking_id");
+                int customer_id1=bookingobj.getInt("customer_id1");
+                int flight_id1=bookingobj.getInt("flight_id1");
+                String class_field=bookingobj.getString("class_field");
 
-        try{
-            statement = DataConnection.getConnection().createStatement();
-            ResultSet rs = statement.executeQuery("SELECT* FROM booking");
+                Booking b = new Booking();
+                b.setBooking_id(booking_id);
+                b.setCustomer_id(customer_id1);
+                b.setFlight_id(flight_id1);
+                b.setFare_class(class_field);
 
-            if(rs != null)
-                while (rs.next()) {
-                    Booking booking = new Booking();
-                    booking.setBooking_id(rs.getInt(1));
-                    booking.setCustomer_id(rs.getInt(2));
-                    booking.setFlight_id(rs.getInt(3));
-                    booking.setFare_class(rs.getString(4));
+                bookings.add(b);
 
-                    bookings.add(booking);
-                }
-        }
-
-        catch(Exception e){
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
         return bookings;
     }
 
@@ -51,7 +69,16 @@ public class BookingData {
     public static void insertBooking(Booking booking)
     {
         try{
-            statement.executeUpdate("INSERT INTO booking VALUE(default, " + booking.getCustomer_id() + ", " + booking.getFlight_id() + ", '" + booking.getFare_class() + "');");
+            HttpClient client = HttpClient.newHttpClient();
+            String input="{ \"customer_id1\":\""+booking.getCustomer_id()+"\", \"flight_id1\":\""+booking.getFlight_id()+"\", \"class_field\":\""+booking.getFare_class()+"\" }";
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(input))
+                    .build();
+
+            HttpResponse<String> response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());
         }
 
         catch(Exception e){
@@ -64,7 +91,17 @@ public class BookingData {
     //method to edit a booking
     public static void updateBooking(Booking booking){
         try{
-            statement.executeUpdate("UPDATE booking SET customer_id1 = " + booking.getCustomer_id() + ", flight_id1 = " + booking.getFlight_id() + ", class = '" + booking.getFare_class() + "' WHERE booking_id = " + booking.getBooking_id() + ";" );
+            String urlpatch = url+booking.getBooking_id()+"/";
+            HttpClient client = HttpClient.newHttpClient();
+            String input="{ \"customer_id1\":\""+booking.getCustomer_id()+"\", \"flight_id1\":\""+booking.getFlight_id()+"\", \"class_field\":\""+booking.getFare_class()+"\" }";
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(urlpatch))
+                    .method("PATCH", HttpRequest.BodyPublishers.ofString(input))
+                    .header("Content-Type", "application/json")
+                    .build();
+
+            HttpResponse<String> response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());
         }
 
         catch(Exception e){
